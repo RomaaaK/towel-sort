aaa671e574a9899b81d73c759ee23bbbe78505d3
RomaaaK <zimpsonzya@gmail.com>
2022-01-25 19:35:45 +0300

    feat: realiz function towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return []
+  const result = []
+  matrix.forEach((e, i) => {
+    const currentElement = (i % 2 === 0) ? e : e.reverse()
+    result.push(...currentElement)
+  });
+  return result;
 }
